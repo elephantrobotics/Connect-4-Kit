b8d18f7dab1d8f7ea6ca39e55d714d8b86f1d16c
roggeohta <roggeohta@outlook.com>
2023-11-15 11:01:16 +0800

    refactoring ArmInterface.py

--- core/ArmInterface.py
+++ core/ArmInterface.py
@@ -1,5 +1,5 @@
 import time
-from pymycobot import MyCobot
+from pymycobot import MyCobot, MyArm
 from typing import *
 import logging
 from core.logger import get_logger
@@ -8,16 +8,13 @@ logger = get_logger(__name__, filepath="logs/robot.log")
 
 
 class ArmInterface:
-    def __init__(self, port: str, baudrate: int):
+    def __init__(self, port: str, baudrate: int, model : str):
         # Define arm speeds
         self.ARM_SPEED = 60
         self.ARM_SPEED_PRECISE = 10
         self.port = port
-        # Define angle tables for different positions
-        self.angle_table = {
-            "recovery": [0, 0, 0, 0, 0, 45],
-            "observe": [-53.78, 116.98, -117.86, -29.35, 52.38, 20.03],
-        }
+        
+        self.init_angle_constants()
 
         # Define chess table for different positions
         self.chess_table = [None for _ in range(7)]
@@ -33,7 +30,10 @@ class ArmInterface:
         self.retry = 5
 
         # Initialize MyCobot instance
-        self.mc = MyCobot(port, baudrate, timeout=0.5, debug=True)
+        if model == "MyCobot":
+            self.mc = _MyCobot(port, baudrate)
+        elif model == "MyArm":
+            self.mc = _MyArm(port, baudrate)
 
         # counter mycobot module contaminating the root logger
         # logging.getLogger().setLevel(logging.CRITICAL)
@@ -49,14 +49,6 @@ class ArmInterface:
 
         self.mc.set_fresh_mode(0)
 
-    # Method to turn on the pump
-    def pump_on(self):
-        # Open the solenoid valve
-        time.sleep(0.05)
-        self.mc.set_basic_output(5, 0)
-        time.sleep(0.05)
-        time.sleep(1)
-
     # Method to send angles with retry logic
     def send_angles(self, angle, speed):
         self.mc.send_angles(angle, speed)
@@ -68,19 +60,6 @@ class ArmInterface:
     # Method to send coordinates with retry logic
     def send_coord(self, arm_id, coord, speed):
         self.mc.send_coord(arm_id, coord, speed)
-
-    # Method to turn off the pump
-    def pump_off(self):
-        # Close the solenoid valve
-        time.sleep(0.05)
-        self.set_basic_output(5, 1)
-        time.sleep(0.1)
-        # Start the exhaust valve
-        self.set_basic_output(2, 0)
-        time.sleep(1)
-        self.set_basic_output(2, 1)
-        time.sleep(0.05)
-        time.sleep(1)
 
     # Method to return to the initial position
     def recovery(self):
@@ -121,3 +100,48 @@ class ArmInterface:
         logger.debug(f"Dropping piece at {self.mc.get_angles()}")
         self.mc.move_round()
         time.sleep(3)
+
+    def init_angle_constants():
+        raise NotImplementedError()
+    
+class _MyCobot(ArmInterface):
+    def __init__(self, port: str, baudrate: int):
+        super(port, baudrate)
+
+    def init_angle_constants():
+        # Define angle tables for different positions
+        self.angle_table = {
+            "recovery": [0, 0, 0, 0, 0, 45],
+            "observe": [-53.78, 116.98, -117.86, -29.35, 52.38, 20.03],
+        }
+        
+        # Define chess table for different positions
+        self.chess_table = [None for _ in range(7)]
+        self.chess_table[6] = [-23.11, -49.92, -58.35, 96.85, 29.0, 45.08]
+        self.chess_table[5] = [-13.97, -39.37, -77.6, 105.82, 19.86, 46.05]
+        self.chess_table[4] = [-2.46, -32.25, -91.31, 115.4, 2.81, 48.33]
+        self.chess_table[3] = [12.3, -29, -104.41, 126.65, -12.91, 49.21]
+        self.chess_table[2] = [20.74, -29.79, -100.63, 127.35, -17.31, 50.44]
+        self.chess_table[1] = [33.57, -23.9, -103.88, 116.63, -29.09, 56.86]
+        self.chess_table[0] = [45.96, -26.45, -98.87, 111.79, -45.08, 59.76]
+
+class _MyArm(ArmInterface):
+    def __init__(self, port : str, baudrate : int):
+        super(port, baud)
+    
+    def init_angle_constants():
+        # Define angle tables for different positions
+        self.angle_table = {
+            "recovery": [0, 0, 0, 0, 0, 45],
+            "observe": [-53.78, 116.98, -117.86, -29.35, 52.38, 20.03],
+        }
+        
+        # Define chess table for different positions
+        self.chess_table = [None for _ in range(7)]
+        self.chess_table[6] = [-23.11, -49.92, -58.35, 96.85, 29.0, 45.08]
+        self.chess_table[5] = [-13.97, -39.37, -77.6, 105.82, 19.86, 46.05]
+        self.chess_table[4] = [-2.46, -32.25, -91.31, 115.4, 2.81, 48.33]
+        self.chess_table[3] = [12.3, -29, -104.41, 126.65, -12.91, 49.21]
+        self.chess_table[2] = [20.74, -29.79, -100.63, 127.35, -17.31, 50.44]
+        self.chess_table[1] = [33.57, -23.9, -103.88, 116.63, -29.09, 56.86]
+        self.chess_table[0] = [45.96, -26.45, -98.87, 111.79, -45.08, 59.76]
