3996367f5c119c43676ef9fa8395a2bc9aa77f78
RoggeOhta <roggeohta@outlook.com>
2023-11-24 19:05:02 +0800

    update translation

--- assets/locales/en.ts
+++ assets/locales/en.ts
@@ -15,164 +15,179 @@
         <translation>Connect-4 Kit</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="97"/>
+        <location filename="../../layouts/app_page.ui" line="141"/>
         <source>串口选择</source>
         <translation>Serial port selection</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="240"/>
+        <location filename="../../layouts/app_page.ui" line="284"/>
         <source>相机选择</source>
         <translation>Camera selection</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="332"/>
-        <location filename="../../layouts/app_page.ui" line="823"/>
+        <location filename="../../layouts/app_page.ui" line="376"/>
+        <location filename="../../layouts/app_page.ui" line="867"/>
         <source>默认视频流</source>
         <translation>Default Video</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="365"/>
+        <location filename="../../layouts/app_page.ui" line="409"/>
         <source>灰度流</source>
         <translation>Gray Video</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="199"/>
-        <location filename="../../layouts/app_page.ui" line="404"/>
+        <location filename="../../layouts/app_page.ui" line="243"/>
+        <location filename="../../layouts/app_page.ui" line="448"/>
         <source>启动</source>
         <translation>Start</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="142"/>
-        <location filename="../../layouts/app_page.ui" line="283"/>
+        <location filename="../../layouts/app_page.ui" line="95"/>
+        <source>机型选择</source>
+        <translation>Model Selection</translation>
+    </message>
+    <message>
+        <location filename="../../layouts/app_page.ui" line="123"/>
+        <source>MyCobot</source>
+        <translation>MyCobot</translation>
+    </message>
+    <message>
+        <location filename="../../layouts/app_page.ui" line="128"/>
+        <source>MyArm</source>
+        <translation>MyArm</translation>
+    </message>
+    <message>
+        <location filename="../../layouts/app_page.ui" line="186"/>
+        <location filename="../../layouts/app_page.ui" line="327"/>
         <source>0</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="147"/>
-        <location filename="../../layouts/app_page.ui" line="288"/>
+        <location filename="../../layouts/app_page.ui" line="191"/>
+        <location filename="../../layouts/app_page.ui" line="332"/>
         <source>1</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="152"/>
-        <location filename="../../layouts/app_page.ui" line="293"/>
+        <location filename="../../layouts/app_page.ui" line="196"/>
+        <location filename="../../layouts/app_page.ui" line="337"/>
         <source>2</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="157"/>
-        <location filename="../../layouts/app_page.ui" line="298"/>
+        <location filename="../../layouts/app_page.ui" line="201"/>
+        <location filename="../../layouts/app_page.ui" line="342"/>
         <source>3</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="162"/>
-        <location filename="../../layouts/app_page.ui" line="303"/>
+        <location filename="../../layouts/app_page.ui" line="206"/>
+        <location filename="../../layouts/app_page.ui" line="347"/>
         <source>4</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="167"/>
-        <location filename="../../layouts/app_page.ui" line="308"/>
+        <location filename="../../layouts/app_page.ui" line="211"/>
+        <location filename="../../layouts/app_page.ui" line="352"/>
         <source>5</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="172"/>
-        <location filename="../../layouts/app_page.ui" line="313"/>
+        <location filename="../../layouts/app_page.ui" line="216"/>
+        <location filename="../../layouts/app_page.ui" line="357"/>
         <source>6</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="218"/>
-        <location filename="../../layouts/app_page.ui" line="423"/>
+        <location filename="../../layouts/app_page.ui" line="262"/>
+        <location filename="../../layouts/app_page.ui" line="467"/>
         <source>停止</source>
         <translation>Stop</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="447"/>
+        <location filename="../../layouts/app_page.ui" line="491"/>
         <source>对弈模型选择</source>
         <translation>Agent Select</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="486"/>
+        <location filename="../../layouts/app_page.ui" line="530"/>
         <source>官方模型</source>
         <translation>Default</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="505"/>
+        <location filename="../../layouts/app_page.ui" line="549"/>
         <source>自定义模型</source>
         <translation>DIY</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="522"/>
+        <location filename="../../layouts/app_page.ui" line="566"/>
         <source>模型</source>
         <translation>Model</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="561"/>
+        <location filename="../../layouts/app_page.ui" line="605"/>
         <source>设定对弈规则</source>
         <translation>Game Rules</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="594"/>
+        <location filename="../../layouts/app_page.ui" line="638"/>
         <source>机器人先手</source>
         <translation>Robot first</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="627"/>
+        <location filename="../../layouts/app_page.ui" line="671"/>
         <source>对弈等待(秒)</source>
         <translation>Waiting Time(s)</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="657"/>
+        <location filename="../../layouts/app_page.ui" line="701"/>
         <source>等待次数限制</source>
         <translation>Max Wait Times</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="694"/>
-        <location filename="../../layouts/app_page.ui" line="904"/>
+        <location filename="../../layouts/app_page.ui" line="738"/>
+        <location filename="../../layouts/app_page.ui" line="948"/>
         <source>识别标注视频流</source>
         <translation>Recognize Video</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="727"/>
-        <location filename="../../layouts/app_page.ui" line="940"/>
+        <location filename="../../layouts/app_page.ui" line="771"/>
+        <location filename="../../layouts/app_page.ui" line="984"/>
         <source>算法结果展示</source>
         <translation>Algorithm Video</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="845"/>
+        <location filename="../../layouts/app_page.ui" line="889"/>
         <source>Video0</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="868"/>
+        <location filename="../../layouts/app_page.ui" line="912"/>
         <source>灰度视频流</source>
         <translation>Gray Video</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="884"/>
+        <location filename="../../layouts/app_page.ui" line="928"/>
         <source>Video1</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="920"/>
+        <location filename="../../layouts/app_page.ui" line="964"/>
         <source>Video2</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="956"/>
+        <location filename="../../layouts/app_page.ui" line="1000"/>
         <source>Video3</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="1260"/>
+        <location filename="../../layouts/app_page.ui" line="1304"/>
         <source>下棋装置调试</source>
         <translation>Piece Dropping Equipment Test</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="1299"/>
+        <location filename="../../layouts/app_page.ui" line="1343"/>
         <source>转动一圈</source>
         <translation>Rotate One Cycle</translation>
     </message>
@@ -181,42 +196,42 @@
         <translation type="obsolete">Go back</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="1042"/>
+        <location filename="../../layouts/app_page.ui" line="1086"/>
         <source>机器人调试开关</source>
         <translation>Robot Test</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="1081"/>
+        <location filename="../../layouts/app_page.ui" line="1125"/>
         <source>打开</source>
         <translation>Turn On</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="1100"/>
+        <location filename="../../layouts/app_page.ui" line="1144"/>
         <source>关闭</source>
         <translation>Turn Off</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="1121"/>
+        <location filename="../../layouts/app_page.ui" line="1165"/>
         <source>移动路径点位调试</source>
         <translation>Moving Path</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="1175"/>
+        <location filename="../../layouts/app_page.ui" line="1219"/>
         <source>移动至该点</source>
         <translation>Move To</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="1194"/>
+        <location filename="../../layouts/app_page.ui" line="1238"/>
         <source>修改点位</source>
         <translation>Change</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="1220"/>
+        <location filename="../../layouts/app_page.ui" line="1264"/>
         <source>保存点位</source>
         <translation>Save</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="1239"/>
+        <location filename="../../layouts/app_page.ui" line="1283"/>
         <source>恢复默认</source>
         <translation>Reset</translation>
     </message>
@@ -225,12 +240,12 @@
         <translation type="obsolete">Pump Test</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="1354"/>
+        <location filename="../../layouts/app_page.ui" line="1398"/>
         <source>开始对弈</source>
         <translation>Start</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.ui" line="1384"/>
+        <location filename="../../layouts/app_page.ui" line="1428"/>
         <source>结束对弈</source>
         <translation>Stop</translation>
     </message>
@@ -274,52 +289,62 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../../layouts/app_page.py" line="303"/>
         <location filename="../../layouts/app_page.py" line="306"/>
-        <location filename="../../layouts/app_page.py" line="400"/>
+        <location filename="../../layouts/app_page.py" line="309"/>
+        <location filename="../../layouts/app_page.py" line="430"/>
         <source>错误</source>
         <translation>Error</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.py" line="303"/>
-        <location filename="../../layouts/app_page.py" line="378"/>
+        <location filename="../../layouts/app_page.py" line="306"/>
+        <location filename="../../layouts/app_page.py" line="408"/>
         <source>请先连接机械臂</source>
         <translation>Please connect arm first</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.py" line="306"/>
+        <location filename="../../layouts/app_page.py" line="309"/>
         <source>请先连接相机</source>
         <translation>Please connect camera first</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.py" line="448"/>
+        <location filename="../../layouts/app_page.py" line="478"/>
         <source>等待线程退出</source>
         <translation>Waiting for threads to exit</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.py" line="349"/>
-        <location filename="../../layouts/app_page.py" line="367"/>
-        <location filename="../../layouts/app_page.py" line="378"/>
+        <location filename="../../layouts/app_page.py" line="374"/>
+        <location filename="../../layouts/app_page.py" line="397"/>
+        <location filename="../../layouts/app_page.py" line="408"/>
         <source>成功</source>
         <translation>Success</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.py" line="349"/>
+        <location filename="../../layouts/app_page.py" line="375"/>
         <source>机械臂连接成功</source>
         <translation>Arm connected</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.py" line="367"/>
+        <location filename="../../layouts/app_page.py" line="380"/>
+        <source>失败</source>
+        <translation>Failed</translation>
+    </message>
+    <message>
+        <location filename="../../layouts/app_page.py" line="381"/>
+        <source>机械臂连接失败, 请检查机型和串口是否匹配</source>
+        <translation>Robot arm connect failed, please check if serial port matched.</translation>
+    </message>
+    <message>
+        <location filename="../../layouts/app_page.py" line="397"/>
         <source>机械臂连接已关闭</source>
         <translation>Arm released</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.py" line="400"/>
+        <location filename="../../layouts/app_page.py" line="430"/>
         <source>请先选择相机编号</source>
         <translation>Please choose camera index first</translation>
     </message>
     <message>
-        <location filename="../../layouts/app_page.py" line="411"/>
+        <location filename="../../layouts/app_page.py" line="441"/>
         <source>等待相机启动</source>
         <translation>Waitting for camera to open</translation>
     </message>
